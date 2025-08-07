4f987017de7ef482a9a1bc3830d280ab67356d1e
Muhammad Saad <saad.swl786@gmail.com>
2025-08-07 21:55:46 +0500

    firebase setup

--- lib/shared/utils/responsive_utils.dart
+++ lib/shared/utils/responsive_utils.dart
@@ -4,8 +4,9 @@ class Responsive {
   static late MediaQueryData _mediaQueryData;
   static late double screenWidth;
   static late double screenHeight;
-  static late double blockWidth;
-  static late double blockHeight;
+
+  static late double blockWidth;   // 1% of screen width
+  static late double blockHeight;  // 1% of screen height
 
   static void init(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
@@ -15,15 +16,15 @@ class Responsive {
     blockHeight = screenHeight / 100;
   }
 
-  static double height(double value) => screenHeight * (value / 812);
+  static double heightPercent(double percent) => blockHeight * percent;
 
-  static double width(double value) => screenWidth * (value / 375);
+  static double widthPercent(double percent) => blockWidth * percent;
 
-  static double fontSize(double value) => screenWidth * (value / 375);
+  static double fontSize(double percent) => blockWidth * percent;
 
-  static double radius(double value) => screenWidth * (value / 375);
+  static double radius(double percent) => blockWidth * percent;
 
-  static double padding(double value) => screenWidth * (value / 375);
+  static double padding(double percent) => blockWidth * percent;
 
-  static double margin(double value) => screenWidth * (value / 375);
+  static double margin(double percent) => blockWidth * percent;
 }
